eb1f9937561534f20856820a02b9d9b0d9f98fab
nullabee (Nick Sabbe) <nick.sabbe@ugent.be>
2011-08-24 20:09:45 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/addendum@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/addendum/R/Diagnostics.r
+++ pkg/addendum/R/Diagnostics.r
@@ -1,20 +1,3 @@
-demoMemUsage1<-function()
-{
-	#list memory size of one object:
-	object.size
-
-	memory.limit()
-	#How many megabytes are available to R at the most
-	#You can increase this, but only to a limit (on 32bit windows typically 2 Gb = 2047)
-	#by passing a number of megabytes. Note: you cannot lower this!
-	memory.size(TRUE)
-	#Size of the biggest possible object you can allocate memory for
-	#in current version of R (in Mb)
-	memory.size()
-	#How much memory is currently actually in use
-	gc(verbose=TRUE)
-	#clean up unused memory
-}
 
 #list memory sizes of all non-function objects present in memory
 	#threshold<-100
