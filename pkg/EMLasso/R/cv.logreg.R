509354decefd5ac6ca299894138d74d827c9d840
nullabee (Nick Sabbe) <nick.sabbe@ugent.be>
2011-10-27 16:05:49 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/addendum@100 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EMLasso/R/cv.logreg.R
+++ pkg/EMLasso/R/cv.logreg.R
@@ -28,6 +28,7 @@ cv.logreg<-function(dfr, resp, wts=rep(1, nrow(dfr)), verbosity=0, useCols=NULL,
 		dfrConvData<-dfrConversionProbs(dfr, "")
 	}
 	dfr.mat<-factorsToDummyVariables(dfr, betweenColAndLevel = "", dfrConvData=dfrConvData, verbosity=verbosity-1) #Some factors are only partially there!
+	firstcolname<-colnames(dfr.mat)[1]
 	catwif(verbosity > 0, "after factorsToDummyVariables dim dfr.mat:", dim(dfr.mat))
 	if(!is.null(useCols))
 	{
@@ -46,28 +47,38 @@ cv.logreg<-function(dfr, resp, wts=rep(1, nrow(dfr)), verbosity=0, useCols=NULL,
 	catwif(verbosity > 0, "dim dfr.mat:", dim(dfr.mat), ", l(resp):", length(resp), ", l(wts):", length(wts), "\n")
 	if(ncol(dfr.mat) == 0)
 	{
-		catwif(verbosity > 0, "No predictors, so returning NULL")
-		return(NULL)
+		catwif(verbosity > 0, "No predictors, so we will do some faking...")
+		dfr.mat<-matrix(runif(nrow(dfr)), ncol=1) #make dfr.mat one random column
+		colnames(dfr.mat)<-firstcolname #just to make sure it is a 'known' column name
+		catwif(verbosity > 5, "dfr.mat structure")
+		if(verbosity > 5) str(dfr.mat)
+		tmpln<-glmnet(dfr.mat, resp, family="binomial", weights=wts, standardize=FALSE)
+		#get the biggest lambda there, and just multiply it by 10
+		useLambda<-10*max(tmpln$lambda) #get huge value for lambda and hope it works to set coef to 0
 	}
-	#debug.tmp<<-list(dfr.mat=dfr.mat, resp=resp, wts=wts)
-	catwif(verbosity > 0, "Checking for NAs in the data")
-	naperrow<-apply(dfr.mat, 1, function(currow){sum(is.na(currow))})
-	whichrowsna<-which(naperrow > 0)
-	if(length(whichrowsna) > 0)
+	else
 	{
-		if(verbosity > 0)
+		#debug.tmp<<-list(dfr.mat=dfr.mat, resp=resp, wts=wts)
+		catwif(verbosity > 0, "Checking for NAs in the data")
+		naperrow<-apply(dfr.mat, 1, function(currow){sum(is.na(currow))})
+		whichrowsna<-which(naperrow > 0)
+		if(length(whichrowsna) > 0)
 		{
-			catw("The following rows have NAs (names are rownumbers, values are nr of NAs):")
-			tmp<-naperrow[whichrowsna]
-			names(tmp)<-whichrowsna
-			print(tmp)
-			catw("These rows will now be removed from the dataset before fitting.")
+			if(verbosity > 0)
+			{
+				catw("The following rows have NAs (names are rownumbers, values are nr of NAs):")
+				tmp<-naperrow[whichrowsna]
+				names(tmp)<-whichrowsna
+				print(tmp)
+				catw("These rows will now be removed from the dataset before fitting.")
+			}
+			dfr.mat<-dfr.mat[-whichrowsna,]
+			resp<-resp[-whichrowsna]
+			wts<-wts[-whichrowsna]
 		}
-		dfr.mat<-dfr.mat[-whichrowsna,]
-		resp<-resp[-whichrowsna]
-		wts<-wts[-whichrowsna]
+		useLambda<-0
 	}
-	fit<-cv.glmnet(dfr.mat, resp, family="binomial", weights=wts, lambda=0, 
+	fit<-cv.glmnet(dfr.mat, resp, family="binomial", weights=wts, lambda=useLambda, 
 		standardize=FALSE, nfolds=nfolds, type.measure=type.measure)
 	catwif(verbosity > 0, "glm fit succeeded.")
 	return(fit)
--- pkg/EMLasso/R/simplyValidate.R
+++ pkg/EMLasso/R/simplyValidate.R
@@ -1,22 +1,22 @@
-#' Crossvalidate a model
+#' Validate a model (to its original data)
 #' 
-#' Crossvalidate a model
+#' Validate a model (to its original data)
 #' 
 #' @param model model fit
 #' @param \dots for flexibility in 'derived' implementation
 #' @param verbosity The higher this value, the more levels of progress and debug 
 #' information is displayed (note: in R for Windows, turn off buffered output)
-#' @note aids to generalize crossvalidation
+#' @note aids to generalize validation
 #' @author Nick Sabbe \email{nick.sabbe@@ugent.be}
-#' @keywords crossvalidate model
+#' @keywords validate model
 #' @export
-crossValidate<-function(model, ..., verbosity=0) UseMethod("crossValidate")
+simplyValidate<-function(model, ..., verbosity=0) UseMethod("simplyValidate")
 
-#' @rdname crossValidate
+#' @rdname simplyValidate
 #' 
-#' @aliases crossValidate.EMLasso.1l.lognet cv.EMLasso.1l.lognet-class cv.EMLasso.1l.lognet
-#' @method crossValidate EMLasso.1l.lognet
-#' @usage \method{crossValidate}{EMLasso.1l.lognet}(model, ds=model$dfr, out=model$resp, wts, dsconvprobs, needPredict=0, betweenColAndLevel="",..., verbosity=0)
+#' @aliases simplyValidate.EMLasso.1l.lognet sv.EMLasso.1l.lognet-class sv.EMLasso.1l.lognet
+#' @method simplyValidate EMLasso.1l.lognet
+#' @usage \method{simplyValidate}{EMLasso.1l.lognet}(model, ds=model$dfr, out=model$resp, wts, dsconvprobs, needPredict=0, betweenColAndLevel="", type.measure="auc", ..., verbosity=0)
 #' @param ds dataset with predictors
 #' @param out vector (binary factor) of outcomes
 #' @param wts vector of weights (defaults to equal weights for all rows)
@@ -24,8 +24,9 @@ crossValidate<-function(model, ..., verbosity=0) UseMethod("crossValidate")
 #' @param needPredict If \code{> 0}, the number of rows that is predicted from the \code{GLoMo}
 #' 	in \code{model} for rows with missing data in \code{ds}
 #' @param betweenColAndLevel see \code{\link{dfrConversionProbs}}
-#' @return object of type "cv.EMLasso.1l.lognet":
-#' \item{cv.logreg }{list of \code{\link{cv.1l.emlasso.reduced}} objects per lambda} 
+#' @param type.measure see \code{\link{cv.glmnet}} - for now, only "auc"" is supported
+#' @return object of type "sv.EMLasso.1l.lognet":
+#' \item{sv.logreg }{\code{\link{cv.glmnet}}-like objects} 
 #' \item{ds }{as passed in or reduced if predicted}
 #' \item{out }{as passed in or extended if predicted}
 #' \item{wts }{as passed in or extended if predicted}
@@ -33,8 +34,8 @@ crossValidate<-function(model, ..., verbosity=0) UseMethod("crossValidate")
 #' @seealso \code{\link{EMLasso.1l.lognet}}, \code{\link{cv.logreg}}
 #' @keywords GLoMo EMLasso
 #' @export
-crossValidate.EMLasso.1l.lognet<-function(model, ds=model$dfr, out=model$resp, 
-	wts, dsconvprobs, needPredict=0, betweenColAndLevel="",..., verbosity=0)
+simplyValidate.EMLasso.1l.lognet<-function(model, ds=model$dfr, out=model$resp, 
+	wts, dsconvprobs, needPredict=0, betweenColAndLevel="", type.measure="auc", ..., verbosity=0)
 {
 	if(missing(dsconvprobs))
 	{
@@ -80,36 +81,68 @@ crossValidate.EMLasso.1l.lognet<-function(model, ds=model$dfr, out=model$resp,
 	{
 		wts<-rep(1, nrow(ds))
 	}
-	
-	logregres<-cv.logreg(dfr=ds, resp=out, wts=wts, verbosity=verbosity-1, useCols=useVarNames, 
-		dfrConvData=dsconvprobs, ...)
-	if(needPredict>0)
+	if(type.measure != "auc")
 	{
-		#make the predicted ds object less memory intensive
-		ds<-reduce(ds, orgdfr=orgds, repsperrow=newdta$numRepPerRow)
+		stop("Other measures than auc are not yet supported in simplyValidate.EMLasso.1l.lognet")
 	}
-	retval<-list(cv.logreg=logregres, ds=ds, out=out, wts=wts, fromLambda=model$lambda)
-	class(retval)<-"cv.EMLasso.1l.lognet"
+	mat<-factorsToDummyVariables(ds, dfrConvData=dsconvprobs)
+	#debug.tmp<<-list(mat=mat, wts=wts, out=out, ds=ds)
+	#nicked these lines from cv.lognet, simplified for 1 "fold" and 1 lambda
+	predmat<-matrix(predict(model$lasso.fit, mat, type = "response"), ncol=1)
+	#debug.tmp$predmat<<-predmat
+	y<-out #to be able to copy code from cv.lognet
+  nc<-dim(y)
+  if (is.null(nc)) {
+      y = as.factor(y)
+      ntab = table(y)
+      nc = as.integer(length(ntab))
+      y = diag(nc)[as.numeric(y), ]
+  }
+	#debug.tmp$y<<-y
+	cvraw<-matrix(auc.mat(y, predmat[, 1, drop=FALSE], wts))
+	#debug.tmp$cvraw<<-cvraw
+	#good<-matrix(1, 1, 1)
+	N<-1 #?? I think this is the right translation of the if (type.measure == "auc") part in cv.lognet
+	weights<-sum(wts) #yikes!
+
+  cvm<-apply(cvraw, 2, weighted.mean, w = weights, na.rm = TRUE)
+  cvsd<-sqrt(apply(scale(cvraw, cvm, FALSE)^2, 2, weighted.mean, 
+      w = weights, na.rm = TRUE)/(N - 1)) #oops, this will surely fail...
+  typenames<-c(mse = "Mean-Squared Error", mae = "Mean Absolute Error", 
+      deviance = "Binomial Deviance", auc = "AUC", class = "Misclassification Error")
+  name<-typenames[type.measure]
+
+	result<-list(lambda=model$lambda, cvm=cvm, cvsd=cvsd, cvup=cvm+cvsd, cvlo=cvm-cvsd,
+		nzero=length(useVarIndexes), name=name, glmnet.fit=model, lambda.min=model$lambda, 
+		lambda.1se=model$lambda)
+	class(result)<-c("cv.glmnet", "cv.lognet")
+ 	if(needPredict>0)
+ 	{
+ 		#make the predicted ds object less memory intensive
+ 		ds<-reduce(ds, orgdfr=orgds, repsperrow=newdta$numRepPerRow)
+ 	}
+	retval<-list(sv.logreg=result, ds=ds, out=out, wts=wts, fromLambda=model$lambda)
+	class(retval)<-"sv.EMLasso.1l.lognet"
 	return(retval)
 }
+
 		 
-#' @rdname crossValidate
+#' @rdname simplyValidate
 #' 
-#' @aliases crossValidate.EMLasso.lognet cv.EMLasso.lognet-class cv.EMLasso.lognet
-#' @method crossValidate EMLasso.lognet
-#' @usage \method{crossValidate}{EMLasso.lognet}(model, ds=model$result[[1]]$dfr, out=model$result[[1]]$resp, wts=rep(1, nrow(ds)), dsconvprobs, needPredict=0, betweenColAndLevel="",..., type.measure="auc", keepResultPerLambda=FALSE, verbosity=0)
-#' @param type.measure see \code{\link{cv.glmnet}}
+#' @aliases simplyValidate.EMLasso.lognet sv.EMLasso.lognet-class sv.EMLasso.lognet
+#' @method simplyValidate EMLasso.lognet
+#' @usage \method{simplyValidate}{EMLasso.lognet}(model, ds=model$result[[1]]$dfr, out=model$result[[1]]$resp, wts=rep(1, nrow(ds)), dsconvprobs, needPredict=0, betweenColAndLevel="",..., type.measure="auc", keepResultPerLambda=FALSE, verbosity=0)
 #' @param keepResultPerLambda if \code{TRUE} (not the default), the individual results
-#' 	from the \code{crossValidate.EMLasso.1l.lognet} are also returned in an extra item
+#' 	from the \code{simplyValidate.EMLasso.1l.lognet} are also returned in an extra item
 #' 	\code{resultPerLambda}
-#' @return object of type "cv.EMLasso.lognet". This is mainly the same as a \code{\link{cv.glmnet}}.
+#' @return object of type "sv.EMLasso.lognet". This is mainly the same as a \code{\link{cv.glmnet}}.
 #' The added/altered items are:
 #' \item{glmnet.fit }{is now the model passed in, so of class "EMLasso.lognet", besides "glmnet"} 
-#' \item{resultPerLambda }{list of "cv.EMLasso.1l.lognet" objects per lambda. Not present if \code{keepResultPerLambda=FALSE}}
+#' \item{resultPerLambda }{list of "sv.EMLasso.1l.lognet" objects per lambda. Not present if \code{keepResultPerLambda=FALSE}}
 #' @seealso \code{\link{EMLasso.1l.lognet}}, \code{\link{cv.logreg}}, \code{\link{cv.glmnet}}
 #' @keywords GLoMo EMLasso
 #' @export
-crossValidate.EMLasso.lognet<-function(model, ds=model$result[[1]]$dfr, out=model$result[[1]]$resp, 
+simplyValidate.EMLasso.lognet<-function(model, ds=model$result[[1]]$dfr, out=model$result[[1]]$resp, 
 	wts=rep(1, nrow(ds)), dsconvprobs, needPredict=0, betweenColAndLevel="",..., type.measure="auc", 
 	keepResultPerLambda=FALSE, verbosity=0)
 {
@@ -123,13 +156,15 @@ crossValidate.EMLasso.lognet<-function(model, ds=model$result[[1]]$dfr, out=mode
 		catwif(verbosity>0, "Passed along betweenColAndLevel does not match the one in dsconvprobs. This last one (", dsconvprobs$betweenColAndLevel, ") will be used.")
 		betweenColAndLevel<-dsconvprobs$betweenColAndLevel
 	}
-	partres<-lapply(model$result, crossValidate, ds=ds, out=out, wts=wts, dsconvprobs=dsconvprobs,
+	partres<-lapply(model$result, simplyValidate, ds=ds, out=out, wts=wts, dsconvprobs=dsconvprobs,
 		needPredict=needPredict, ..., type.measure=type.measure, verbosity=verbosity-1)
-	cvlogreglist<-lapply(partres, "[[", "cv.logreg")
+	cvlogreglist<-lapply(partres, "[[", "sv.logreg")
 	
 	lambda<-model$lambda
 	cvm<-as.vector(unlist(try(sapply(cvlogreglist, "[[", "cvm"))))
-	cvsd<-as.vector(unlist(try(sapply(cvlogreglist, "[[", "cvsd"))))
+	#cvsd<-as.vector(unlist(try(sapply(cvlogreglist, "[[", "cvsd")))) these are all NaN!
+	#so we use the variability over the cvm
+	cvsd<-rep(sd(cvm), length(cvm))
 	nz<-try(sapply(predict(model, type = "nonzero"), length))
 	out<-list(
 		lambda=lambda,
@@ -148,6 +183,6 @@ crossValidate.EMLasso.lognet<-function(model, ds=model$result[[1]]$dfr, out=mode
 	{
 		obj<-c(obj, list(resultPerLambda=partres))
 	}
-	class(obj)<-c("cv.EMLasso.lognet", "cv.glmnet")
+	class(obj)<-c("sv.EMLasso.lognet", "cv.glmnet")
 	return(obj)
 }
