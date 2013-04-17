b1e1dd780f136cbbbb5709025e2b99404365d715
nullabee (Nick Sabbe) <nick.sabbe@ugent.be>
2013-04-17 06:53:33 +0000

    2.0.7: match addendum
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/addendum@216 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/EMLasso/R/EMLasso-package.R
+++ pkg/EMLasso/R/EMLasso-package.R
@@ -5,7 +5,7 @@
 #' Fit (logistic) LASSO when there is missing data in the predictors
 #' 
 #' \tabular{ll}{ Package: \tab EMLasso\cr Type: \tab Package\cr Version: \tab
-#' 2.0.6\cr Date: \tab 2012-10-23\cr License: \tab GPL (>= 2)\cr
+#' 2.0.7\cr Date: \tab 2013-04-17\cr License: \tab GPL (>= 2)\cr
 #' LazyLoad: \tab yes\cr Depends: Matrix, glmnet, snowfall, addendum, NumDfr, GLoMo\cr} 
 #' @name EMLasso-package
 #' @aliases EMLasso-package
